fc871cf0c0c40474e1015283745b6da27e5fd1da
avelard3 <avelard3@asu.edu>
2025-02-03 18:06:06 -0700

    Still trying to figure out optuna. Pushing at end of day

--- src/lib/optuna/hyperparameter_tuning.py
+++ src/lib/optuna/hyperparameter_tuning.py
@@ -52,7 +52,6 @@ def arguments(parser) -> argparse.ArgumentParser:
     
     #Eventually I should make all the hyperparameters inputs too, but not right now
     
-    
     try:
         args = parser.parse_args()
         print("Parsed arguments:")
@@ -69,8 +68,6 @@ def arguments(parser) -> argparse.ArgumentParser:
     
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
-
-
 
 
 
@@ -95,8 +92,8 @@ def define_things(self) -> None:
     self._training_results = args.output
     self._model_dict_save_path = args.dict
     
-    num_epoch = args.epoch
-    batch_size = args.batch
+    self._num_epoch = args.epoch
+    self._batch_size = args.batch
     optimizer = args.optimizer
     scheduler = args.scheduler
     criterion = args.criterion
@@ -107,37 +104,37 @@ def define_things(self) -> None:
     photon_energy_location = args.photon_energy
     peaks_location = args.peaks
     
-    transfer_learning_state_dict = args.transfer_learn
-    transform = args.apply_transform # Parameter for Data class
+    self._transfer_learning_state_dict = args.transfer_learn
+    self._transform = args.apply_transform # Parameter for Data class
     
-    N_TRAIN_EXAMPLES = batch_size * 30
-    N_VALID_EXAMPLES = batch_size * 10
+    self._N_TRAIN_EXAMPLES = self._batch_size * 30
+    self._N_VALID_EXAMPLES = self._batch_size * 10
     #temperary holding
-    transform = False
+    self._transform = False
     
-    master_file = args.master_file
-    if master_file == 'None' or master_file == 'none':
-        master_file = None
+    self._master_file = args.master_file
+    if self._master_file == 'None' or self._master_file == 'none':
+        self._master_file = None
         
         
     # Transfer learning (yes or no)
     # FIXME: There should be a way to get bool to work for this?
     
-    if transfer_learning_state_dict == 'None' or transfer_learning_state_dict == 'none':
-        transfer_learning_state_dict = None
+    if self._transfer_learning_state_dict == 'None' or self._transfer_learning_state_dict == 'none':
+        self._transfer_learning_state_dict = None
     
         
-    h5_locations = {
+    self._h5_locations = {
         'image': image_location,
         'camera length': camera_length_location,
         'photon energy': photon_energy_location,
         'peak': peaks_location
     }
     
-    cfg = {
-        'batch size': batch_size,
+    self._cfg = {
+        'batch size': self._batch_size,
         'device': self._device,
-        'epochs': num_epoch,
+        'epochs': self._num_epoch,
         'optimizer': optimizer,
         'scheduler': scheduler,
         'criterion': criterion,
@@ -166,29 +163,31 @@ def objective(self, trial):
     # learning rate scheduler parameters
     
     executing_mode = 'training' 
-    path_manager = load_paths.Paths(self._h5_file_list, self._h5_locations, executing_mode, master_file) #good?
+    path_manager = load_paths.Paths(self._h5_file_list, self._h5_locations, executing_mode, self._master_file) #good?
 
     path_manager.run_paths() #good?
     
-    training_manager = train_model.TrainModel(cfg, h5_locations, transfer_learning_state_dict)
+    training_manager = train_model.TrainModel(self._cfg, self._h5_locations, self._transfer_learning_state_dict)
     training_manager.make_training_instances()
     training_manager.load_model_state_dict()
 
     vds_dataset = path_manager.get_vds()
     h5_file_paths = path_manager.get_file_names()
     
-    data_manager = load_data.Data(vds_dataset, h5_file_paths, executing_mode, transform, master_file)
+    data_manager = load_data.Data(vds_dataset, h5_file_paths, executing_mode, self._transform, self._master_file)
     
-    create_data_loader = load_data.CreateDataLoader(data_manager, batch_size)
+    create_data_loader = load_data.CreateDataLoader(data_manager, self._batch_size)
 
     create_data_loader.split_training_data() 
     train_loader, test_loader = create_data_loader.get_training_data_loaders() #* #! I made the assumption that test_loader and valid_loader were the same thing
     
-    for epoch in range(num_epoch):
+    
+    #where model is called?
+    for epoch in range(self.from django.utils.translation import ugettext_lazy as _num_epoch):
         model.train()
         for batch_idx, (data, target) in enumerate(train_loader):
             # Limiting training data for faster epochs.
-            if batch_idx * batch_size >= N_TRAIN_EXAMPLES: #I am very unsure what N_TRAIN_EXAMPLES and N_VALID_EXAMPLES are so I'm leaving those for now
+            if batch_idx * self._batch_size >= self._N_TRAIN_EXAMPLES: #I am very unsure what N_TRAIN_EXAMPLES and N_VALID_EXAMPLES are so I'm leaving those for now
                 break
 
             data, target = data.view(data.size(0), -1).to(device), target.to(device)
@@ -205,7 +204,7 @@ def objective(self, trial):
         with torch.no_grad():
             for batch_idx, (data, target) in enumerate(test_loader):
                 # Limiting validation data.
-                if batch_idx * batch_size >= N_VALID_EXAMPLES:
+                if batch_idx * self._batch_size >= self._N_VALID_EXAMPLES:
                     break
                 data, target = data.view(data.size(0), -1).to(device), target.to(device)
                 output = model(data)
@@ -213,7 +212,7 @@ def objective(self, trial):
                 pred = output.argmax(dim=1, keepdim=True)
                 correct += pred.eq(target.view_as(pred)).sum().item()
 
-        accuracy = correct / min(len(test_loader.dataset), N_VALID_EXAMPLES)
+        accuracy = correct / min(len(test_loader.dataset), self._N_VALID_EXAMPLES)
 
         trial.report(accuracy, epoch)
 
@@ -244,35 +243,74 @@ def objective(self, trial):
     evaluation_manager.make_classification_report()
     evaluation_manager.plot_confusion_matrix(training_results)
     evaluation_manager.plot_roc_curve(training_results)
-    
-
-if __name__ == '__main__':
-    main()
 
 
-
-
-
-#! IDK
-def define_model(trial):
+def define_model(self, trial):
     # We optimize the number of layers, hidden units and dropout ratio in each layer.
+    
     n_layers = trial.suggest_int("n_layers", 1, 3)
     layers = []
 
-    in_features = 28 * 28
+    # Assuming that input_channels & output_channels = 1 and input_size = (512, 512)
+    input_channels = 1 # should this include the input_size????
+    output_channels = 1
     for i in range(n_layers):
-        out_features = trial.suggest_int("n_units_l{}".format(i), 4, 128)
-        layers.append(nn.Linear(in_features, out_features))
-        layers.append(nn.ReLU())
-        p = trial.suggest_float("dropout_l{}".format(i), 0.2, 0.5)
-        layers.append(nn.Dropout(p))
+        #*Things that should vary inside this loop*#
+        self.kernel_size = 0
+        self.stride = 0
+        self.pool_kernel_size = 0
+        
+        #**#
+        
+        ouput_channels = trial.suggest_int("n_units_l{}".format(i), 4, 128) #the 4,8,16 variable.... num_conv_channels?
+        layers.append(nn.Conv2d(input_channels, output_channels, kernel_size=self.kernel_size1, stride=self.stride1, padding=self.padding1)) # layers.append(nn.Linear(in_features, out_features))
+        layers.append(F.relu()) #does this need an input?? like relu(x).... is that maybe why it was nn.ReLU or whtever?
+        
+        # p = trial.suggest_float("dropout_l{}".format(i), 0.2, 0.5) #* dropout!!!
+        # layers.append(nn.Dropout(p))
 
-        in_features = out_features
-    layers.append(nn.Linear(in_features, CLASSES))
+        input_channels = output_channels
+    layers.append(nn.Linear(input_channels, CLASSES))
     layers.append(nn.LogSoftmax(dim=1))
 
     return nn.Sequential(*layers)
 
+    self.kernel_size1 = 3
+     self.stride1 = 1
+    self.padding1 = 1
+    self.kernel_size2 = 3
+    self.stride2 = 1
+    self.padding2 = 1
+#below here should just be one time and it can decide the optimal numbwer of layers inside the for loop
+    # self.conv1 = nn.Conv2d(input_channels, 4, kernel_size=self.kernel_size1, stride=self.stride1, padding=self.padding1)
+    self.pool1 = nn.MaxPool2d(2, 2)
+    self.conv2 = nn.Conv2d(4, 8, kernel_size=self.kernel_size2, stride=self.stride2, padding=self.padding2)
+    self.pool2 = nn.MaxPool2d(2,2)
+    self.conv3 = nn.Conv2d(8, 16, kernel_size=self.kernel_size2, stride=self.stride2, padding=self.padding2)
+    self.pool3 = nn.MaxPool2d(2,2)
+    out_height1 = self.calculate_output_dimension(input_size[0], self.kernel_size1, self.stride1, self.padding1)
+    out_width1 = self.calculate_output_dimension(input_size[1], self.kernel_size1, self.stride1, self.padding1)
+    out_height2 = self.calculate_output_dimension(out_height1 // 2, self.kernel_size2, self.stride2, self.padding2)
+    out_width2 = self.calculate_output_dimension(out_width1 // 2, self.kernel_size2, self.stride2, self.padding2)
+    self.fc_size_1 = 0.25 * out_height2 * out_width2 #fully connected layer
+    self.fc1 = nn.Linear(self.fc_size_1, output_channels)
+    def calculate_output_dimension(self, input_dim, kernel_size, stride, padding):
+        return ((input_dim + 2 * padding - kernel_size) // stride) + 1
+    def forward(self, x, camera_length, photon_energy):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool2(x)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.pool3(x)
+        #*this should be outside the for loop
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        return x
 
 
 
